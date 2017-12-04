87f64c762562bd5477b35203262ea136aa79c793
basion96 <jason.horsnell@outlook.com>
2017-12-04 19:58:35 +1100

    Updated the !choose command
    If the choose command is called but no options are given, bot will responed acknowledging that there are no options for it to choose.
    
    If the choose command is called but only 1 option is given, it responds acknowledging that it can only pick 1 option.

--- wholesomeBot.js
+++ wholesomeBot.js
@@ -10,14 +10,14 @@ var wholesomePics = new Array();
 var howDoYouWorkReplies = new Array();
 var lastWholesomeMsg='';
 
-// Initialize Discord Bot
+// Initialize Discord Bot.
 var bot = new Discord.Client({
    token: config.token,
    autorun: true,
    autoReconnect:true
 });
 
-//when the bot is ready, logs to console and fills the arrays
+//when the bot is ready, logs to console and fills the arrays.
 bot.on('ready', function (evt) {
     console.log('Connected');
     console.log('Logged in as: '+ bot.username + ' - (' + bot.id + ')');
@@ -27,10 +27,11 @@ bot.on('ready', function (evt) {
 			type: 0 
 		} 
 	});﻿
+	//fills the arrays.
 	fillArrays();
 });
 
-// Automatically reconnect if the bot disconnects from inactivity
+// Automatically reconnect if the bot disconnects from inactivity.
 bot.on('disconnect', function(erMsg, code) {
     console.log('Bot disconnected: ', code, 'Reason:', erMsg);
     bot.connect();
@@ -39,39 +40,50 @@ bot.on('disconnect', function(erMsg, code) {
 //when a message is sent to the discord
 bot.on('message', function (user, userID, channelID, message, evt) {
 	console.log(user + ": " + message);
+	//checks to see if the message starts with the specified prefix defined in the config file.
     if (message.substring(0, 1) == config.prefix) {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
+			//if command is !wholesome, bot will grab a random wholesome message from the wholesomeReminders array and send it to the same channel as the command.
             case 'wholesome':
             bot.sendMessage({
 				to: channelID,
 				message: wholesomeMessages[getRandom(wholesomeMessages.length)]
 			});
             break;
+			//if command is !byebye, the bot will turn off. Can only be turned on by the person that hosts the bot as the application will need to be restarted.
 			case "byebye":
 				bot.disconnect();
 				process.exit(0);
 			break;
+			//if command is !cheerMeUp, bot will send a message to the same channel as the command to cheer the person up.
 			case "cheerMeUp":
 			bot.sendMessage({
 					to: channelID,
 					message: cheerUpMessages[getRandom(cheerUpMessages.length)]
 				});
 			break;
+			//if command is !updateLists, the arrays will be refreshed and any new messages in them will be added.
 			case "updateLists":
 				console.log("Updating arrays...");
 				fillArrays();
 				console.log("Arrays updated!");
+				bot.sendMessage({
+					to: channelID,
+					message: 'The arrays have been updated :blush:'
+				});
 			break;
+			//if command is !wholesomeImg, bot will send a randomly selected wholesome image to the same channel as the command.
 			case 'wholesomeImg':
 			bot.uploadFile({
 				to: channelID,
 				file: "pictures/"+wholesomePics[getRandom(wholesomePics.length)]
 			});
 			break;
+			//if command is !info, bot will send some basic information about itself.
 			case 'info':
 				bot.sendMessage({
 					to: channelID,
@@ -85,28 +97,45 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					}
 				});
 			break;
+			//if command is !choose and ChoiceOptionss are provided, bot will randomly choose one of the provided ChoiceOptionss and send a message to the channel saying its reponse. 
 			case 'choose':
-				var choiceArray = new Array();
+				var ChoiceOptions = new Array();
 				message=message.substring(message.indexOf('choose')+6);
-				while(message.substring(0,1!="")){
-					if(message.indexOf('|')!=-1){
-						choiceArray.push(message.substring(0, message.indexOf('|')));
+				
+				//if message is not empty and has greater than 1 ChoiceOptions
+				if(message!="" && ChoiceOptions.length>1){
+					while(message.substring(0,1!="")){
+						if(message.indexOf('|')!=-1){
+							ChoiceOptions.push(message.substring(0, message.indexOf('|')));
+						}
+						else{
+							ChoiceOptions.push(message);
+						}
+						
+						if(message.indexOf("|")!=-1){
+							message=message.substring(message.indexOf('|')+1);
+						}
+						else{
+							message="";
+						}
 					}
-					else{
-						choiceArray.push(message);
-					}
-					
-					if(message.indexOf("|")!=-1){
-						message=message.substring(message.indexOf('|')+1);
-					}
-					else{
-						message="";
-					}
+					bot.sendMessage({
+						to: channelID,
+						message: 'I choose: ' + ChoiceOptions[getRandom(ChoiceOptions.length)]
+					});
 				}
-				bot.sendMessage({
-					to: channelID,
-					message: 'I choose: ' + choiceArray[getRandom(choiceArray.length)]
-				});
+				else if(ChoiceOptions.length==1){
+					bot.sendMessage({
+						to: channelID,
+						message: 'I dont really get a choice do i haha, well i guess im gonna choose ' + ChoiceOptions[0] + ' :upside_down: '
+					});
+				}
+				else if(message==""){
+					bot.sendMessage({
+						to: channelID,
+						message: 'Ahhhhhh, i think you forgot to add my ChoiceOptionss :sweat_smile: '
+					});
+				}
 			break;
          }
      }
@@ -162,15 +191,15 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 	 }
 });
 
-function chooseOption(message, channel){
-	var choiceArray = new Array();
+function chooseChoiceOptions(message, channel){
+	var ChoiceOptions = new Array();
 	message=message.substring(message.indexOf('choose')+6);
 	while(message.substring(0,1!="")){
 		if(message.indexOf('|')!=-1){
-			choiceArray.push(message.substring(0, message.indexOf('|')));
+			ChoiceOptions.push(message.substring(0, message.indexOf('|')));
 		}
 		else{
-			choiceArray.push(message);
+			ChoiceOptions.push(message);
 		}
 		
 		if(message.indexOf("|")!=-1){
@@ -180,12 +209,12 @@ function chooseOption(message, channel){
 			message="";
 		}
 	}
-	for(var i=0; i<choiceArray.length; i++){
-		console.log(choiceArray[i]);
+	for(var i=0; i<ChoiceOptions.length; i++){
+		console.log(ChoiceOptions[i]);
 	}
 	bot.sendMessage({
 		to: channelID,
-		message: choiceArray[getRandom(choiceArray.length)]
+		message: ChoiceOptions[getRandom(ChoiceOptions.length)]
 	});
 }
 
