50436e917ed2476c084f4a64dc4cb4cf05181da8
Jason Horsnell <jason.horsnell@outlook.com>
2018-03-02 19:29:01 +1100

    Added more responses
    Added more regex that wholesomebot can respond to

--- wholesomeBot.js
+++ wholesomeBot.js
@@ -257,7 +257,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				});
 		 }
 		 
-		 //if a user says a varitaion of 'i love you too wholesomebot'.
+		 //if a user says a variation of 'i love you too wholesomebot'.
 		 else if(/i love you (to|too) (wholesomebot|<@380542695556251650>|wholesome)/.test(message.toLowerCase())==true){
 			 bot.sendMessage({
 				 to: channelID,
@@ -326,7 +326,55 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 			bot.sendMessage({
 				to:channelID,
 				message: whatAreYouDoingReplies[getRandom(whatAreYouDoingReplies.length)]
-			}); 
+			});
+		 }
+		 
+		 //if message is a variation of 'do you have a crush on anyone wholesomebot'
+		 else if(/(do)? you have a crush on anyone (wholesomebot|<@380542695556251650>|wholesome)\?*/.test(message.toLowerCase())){
+			bot.sendMessage({
+				to:channelID,
+				message: 'I might....\n but i cant\'t tell you because its a secret :wink: '
+			});
+		 }
+		 
+		 //if message is a variation of 'do you believe in love wholesomebot'
+		 else if(/(do)? (you|u) believe in love (wholesomebot|<@380542695556251650>|wholesome)\?*/.test(message.toLowerCase())){
+			bot.sendMessage({
+				to:channelID,
+				message: 'Of course i do! There\'s a perfect match for everyone in this world, whether you know it or not :blush: '
+			});
+		 }
+		 
+		 //if message is a variation of 'who do you like wholesomebot'
+		 else if(/who do (you|u) like (wholesomebot|<@380542695556251650>|wholesome)\?*/.test(message.toLowerCase())){
+			bot.sendMessage({
+				to:channelID,
+				message: 'Who do i like? Hmmmmm, everyone in this discord server!!!'
+			});
+		 }
+		 
+		 //if message is a variation of 'who do you love wholesomebot'
+		 else if(/who do (you|u) love (wholesomebot|<@380542695556251650>|wholesome)\?*/.test(message.toLowerCase())){
+			bot.sendMessage({
+				to:channelID,
+				message: 'I love everyone :blush:  But most of all i love my programmer and those he loves most, I wouldnt be here without them :hearts: '
+			});
+		 }
+		 
+		 //if message is a variation of 'are you an ai wholesomebot'
+		 else if(/are (you|u) (an|a) (ai|AI|a.i.|A.I.) (wholesomebot|<@380542695556251650>|wholesome)\?*/.test(message.toLowerCase())){
+			bot.sendMessage({
+				to:channelID,
+				message: 'haha not at all. Unfortunately i cant exactly "think for myself", every response i give is predetermined and choosen by my programmer. Type the same thing again to see that (spoilers: i\'ll say this again haha)'
+			});
+		 }
+		 
+		//if message is a variation of 'you look good today wholesomebot'
+		else if(/(you|u) look (great|lovely|beautiful|amazing|cute|adorable|nice|good) (right now|atm|today|at the moment) (wholesomebot|<@380542695556251650>|wholesome)\?*/.test(message.toLowerCase())){
+			bot.sendMessage({
+				to:channelID,
+			message: 'Awwwww thanks ' + user + ', you\'re too kind :blush: You\'re looking amazing as always :heart_eyes: '
+			});
 		 }
 	 }
 });
